a6fe672a5a2f636756b357b4ef8426ba15af1dac
adamliewky <adamliewky@gmail.com>
2025-09-30 11:25:18 +0800

    site

--- Scatter_plot.js
+++ Scatter_plot.js
@@ -5,18 +5,16 @@ d3.csv("Ex5/Ex5_TV_energy.csv", d3.autoType).then(data => {
     const height = 400 - margin.top - margin.bottom;
 
     const svg = d3.select("#scatterPlot")
-        .attr("width", width + margin.left + margin.right + 150) // extra space for legend
+        .attr("width", width + margin.left + margin.right + 150)
         .attr("height", height + margin.top + margin.bottom)
         .append("g")
         .attr("transform", `translate(${margin.left},${margin.top})`);
 
-    // Color scale for screen_tech
     const screenTypes = [...new Set(data.map(d => d.screen_tech))];
     const color = d3.scaleOrdinal()
         .domain(screenTypes)
         .range(d3.schemeCategory10);
 
-    // Scales
     const x = d3.scaleLinear()
         .domain(d3.extent(data, d => d.star2))
         .range([0, width]);
@@ -24,14 +22,12 @@ d3.csv("Ex5/Ex5_TV_energy.csv", d3.autoType).then(data => {
         .domain(d3.extent(data, d => d.energy_consumpt))
         .range([height, 0]);
 
-    // Axes
     svg.append("g")
         .attr("transform", `translate(0,${height})`)
         .call(d3.axisBottom(x));
     svg.append("g")
         .call(d3.axisLeft(y));
 
-    // Axis labels
     svg.append("text")
         .attr("x", width / 2)
         .attr("y", height + 40)
@@ -44,7 +40,18 @@ d3.csv("Ex5/Ex5_TV_energy.csv", d3.autoType).then(data => {
         .attr("text-anchor", "middle")
         .text("Energy Consumption (kWh)");
 
-    // Points colored by screen_tech
+    // Tooltip
+    const tooltip = d3.select("body").append("div")
+        .attr("class", "donut-tooltip")
+        .style("position", "absolute")
+        .style("background", "#fff")
+        .style("border", "1px solid #ccc")
+        .style("padding", "8px")
+        .style("border-radius", "4px")
+        .style("pointer-events", "none")
+        .style("display", "none");
+
+    // Points with tooltip
     svg.selectAll("circle")
         .data(data)
         .enter()
@@ -53,7 +60,25 @@ d3.csv("Ex5/Ex5_TV_energy.csv", d3.autoType).then(data => {
         .attr("cy", d => y(d.energy_consumpt))
         .attr("r", 4)
         .attr("fill", d => color(d.screen_tech))
-        .attr("opacity", 0.7);
+        .attr("opacity", 0.7)
+        .on("mouseover", function(event, d) {
+            tooltip.style("display", "block")
+                .html(
+                    `<strong>${d.screen_tech}</strong><br>
+                    Star Rating: ${d.star2}<br>
+                    Energy: ${d.energy_consumpt} kWh<br>
+                    Date: ${d.date ? d.date : "N/A"}`
+                );
+            d3.select(this).attr("stroke", "#333").attr("stroke-width", 2);
+        })
+        .on("mousemove", function(event) {
+            tooltip.style("left", (event.pageX + 10) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function() {
+            tooltip.style("display", "none");
+            d3.select(this).attr("stroke", null);
+        });
 
     // Legend
     const legend = svg.append("g")
@@ -65,7 +90,6 @@ d3.csv("Ex5/Ex5_TV_energy.csv", d3.autoType).then(data => {
             .attr("cy", i * 25)
             .attr("r", 7)
             .attr("fill", color(type));
-
         legend.append("text")
             .attr("x", 18)
             .attr("y", i * 25 + 5)
